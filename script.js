f3cb469624f0659fe622614f7e4e9377611e349b
blinhart5 <blinhart5@gmail.com>
2022-02-13 18:07:09 -0600

    Created functions to build grid, update grid size based on user input, and added hover effect on divs for proper 'etch-a-sketch' effect.

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-console.log("welcome to the Thunderdome"); 
+const body = document.querySelector('body'); 
+const container = document.getElementById('container');
+const clearBtn = document.getElementById('clear');  
+
+clearBtn.addEventListener('click', () => {
+    container.innerText = "";
+    pixels = window.prompt('What size grid would you like? Max = 100');
+    updateGrid();
+    createGrid();
+
+  
+})
+
+
+let pixels = 16;
+createGrid();
+
+function createGrid() {
+    for (let i=0; i < pixels*pixels; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    container.appendChild(square);
+    square.addEventListener('mouseover', () => {
+    square.classList.add('black'); 
+    })
+   
+    }
+    
+}
+
+function updateGrid() {
+    container.style.gridTemplateColumns = `repeat(${pixels}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${pixels}, 1fr)`;
+}
+
+
+
+
+console.log('wu-tang');
+console.log(pixels); 
+
+
+
+
