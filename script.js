e0cc004f8913595f453215fc4eeeb04f638d8951
blinhart5 <blinhart5@gmail.com>
2022-03-12 11:37:32 -0600

    working etch a sketch with random, black and eraser updated. css updated

--- script.js
+++ script.js
@@ -1,43 +1,91 @@
 const body = document.querySelector('body'); 
 const container = document.getElementById('container');
-const clearBtn = document.getElementById('clear');  
+const clearBtn = document.getElementById('clear'); 
+const randomBtn = document.getElementById('random'); 
+const blackBtn = document.getElementById('black');
+const greyScaleBtn = document.getElementById('greyScale')
+const colorBtns = document.querySelectorAll('.colorBtn');
 
-clearBtn.addEventListener('click', () => {
-    container.innerText = "";
-    pixels = window.prompt('What size grid would you like? Max = 100');
-    updateGrid();
-    createGrid();
+/* Adds event listener to buttons that changes the color of the grid on hover/mouseover */
 
-  
-})
+colorBtns.forEach(colorBtn => colorBtn.addEventListener('click', changeColor));
 
+/* Default settings for page loadup */
 
+let color = 'black'; 
 let pixels = 16;
 createGrid();
 
-function createGrid() {
-    for (let i=0; i < pixels*pixels; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
-    square.addEventListener('mouseover', () => {
-    square.classList.add('black'); 
-    })
-   
-    }
-    
-}
+/* Clears grid and prompts user for new size. */
+
+clearBtn.addEventListener('click', () => {
+    container.innerText= "";
+        pixels = window.prompt("Size of Grid?")
+        updateGrid();
+        createGrid();
+})
+
+/* Updates grid-template-columns in css based on default/prompted user input. */
 
 function updateGrid() {
     container.style.gridTemplateColumns = `repeat(${pixels}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${pixels}, 1fr)`;
 }
 
+/* Creates grid by adding divs based on default/prompted user input */
+
+function createGrid() {
+    for (let i=0; i < pixels*pixels; i++) {
+    let gridSquare = document.createElement('div');
+    gridSquare.classList.add('square');
+    container.appendChild(gridSquare);
+   }
+
+   /* Adds event listener to grid divs to change color when mouse moves/hovers over them */
+
+    let gridPixels = container.querySelectorAll('div');
+    gridPixels.forEach(gridPixel => gridPixel.addEventListener('mouseover', colorGrid));
+}
+
+/* Changes color variable based on the id of which button is clicked */
+
+function changeColor (event) {
+    switch (event.target.id) {
+        case 'random': 
+            color = 'random'; 
+            break; 
+        case 'black':
+            color = 'black'; 
+            break; 
+        case 'eraser': 
+            color = 'eraser';
+            break; 
+    }
+}
+
+/* updates the background of the grid pixels based on which color button was clicked */
+
+function colorGrid () {
+    switch (color) { 
+        case 'random': 
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+            break; 
+        case 'black': 
+            this.style.backgroundColor = `hsl(0, 0%, 0%)`;
+            break; 
+        case 'eraser': 
+            this.style.backgroundColor = `hsl(0, 50%, 100%)`; 
+            break;      
+    }
+}
 
 
 
-console.log('wu-tang');
-console.log(pixels); 
+
+ 
+
+
+
 
 
 
